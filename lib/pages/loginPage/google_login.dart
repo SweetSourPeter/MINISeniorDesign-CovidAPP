34449091201ce884d43de5fa756d4e02b3b279bd
labmem008 <yukiyukimu.snow@gmail.com>
2020-09-14 14:05:31 -0400

    google sign in (not done yet)

---
+++ lib/pages/loginPage/google_login.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:covidapp/service/auth.dart';
+
+// this file is for google login (user view) only
+
+class LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: authService.user,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return MaterialButton(
+            color: Colors.red[200],
+            child: Text(
+              'Sign Out',
+              style: TextStyle(color: Colors.black),
+            ),
+            onPressed: () {
+              authService.signOut();
+            },
+          );
+        } else {
+          return MaterialButton(
+            color: Colors.yellow[300],
+            child: Text(
+              'Google Sign In',
+              style: TextStyle(color: Colors.blue),
+            ),
+            onPressed: () {
+              authService.googleSignIn();
+            },
+          );
+        }
+      },
+    );
+  }
+}
